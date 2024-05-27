d5c02f8e333cb03964fa5b370410afcb6690dd33
tushar27 <tushar.27041994@gmail.com>
2024-05-27 13:54:30 +0530

    restructure

--- Home.py
+++ Home.py
@@ -1,6 +1,26 @@
 ##© 2024 Tushar Aggarwal. All rights reserved.(https://tushar-aggarwal.com)
 ##Botimus[Towards-GenAI] (https://github.com/Towards-GenAI)
 ##################################################################################################
+#Importing dependencies
+import streamlit as st
+from pathlib import Path
+import base64
+import sys
+from pathlib import Path
+script_dir = Path(__file__).resolve().parent
+project_root = script_dir.parent
+sys.path.append(str(project_root))
+import warnings
+warnings.filterwarnings("ignore")
+import os
+import logging
+
+#Importing from SRC
+
+
+
+
+##################################################################################################
 
 
 
